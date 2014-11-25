87a4f8cc6539cc172e0c1addf3c36842c3909401
smilecc <sxcuican@163.com>
2014-11-25 14:01:08 +0800

    Project Init

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include "MyGlobalShortCut/MyGlobalShortCut.h"
+#include <QSystemTrayIcon>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    // 用法
+
+    MyGlobalShortCut *shortcut = new MyGlobalShortCut("Alt+Space",&w);
+    QObject::connect(shortcut,SIGNAL(activated()),&w,SLOT(activated()));
+
+    QSystemTrayIcon systray;
+    systray.setIcon(QIcon(":/f.ico"));
+    systray.setVisible(true);
+
+    return a.exec();
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,27 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QMessageBox"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    ishide(false)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::activated()
+{
+    //通过热键控制窗口的显示与隐藏
+    if(!ishide){
+        this->hide();
+        ishide = true;
+    }else{
+        this->show();
+        ishide = false;
+    }
+}
